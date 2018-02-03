e356cf76e71d2201ac6e0e542f80f50ce3d48614
XSingularity <omarcorporated@gmail.com>
2018-02-03 09:37:36 -0400

    Argparse added

--- notes.py
+++ notes.py
@@ -1,19 +1,21 @@
+import argparse
 import base64
 import shelve
 import tkinter as tk
 from binascii import Error as binasciiError
 from datetime import datetime
-from dbm import error as dbmError
+from dbm import error
 from hashlib import sha256
 from os import mkdir
 
 from Crypto import Random
 from Crypto.Cipher import AES
 
-__all__ = ['Note', 'new_note', 'modify_note', 'search_note', 'show_all_notes', 'del_note', 'del_all',
-            'usage', 'AESCipher', 'dbmError']
+__all__ = ['dbmError', 'argparse', 'CreateNote', 'CreateEncryptedNote', 'DecryptNote', 'ModifyNote', 'SearchNote',
+           'ListAll', 'RemoveNote', 'RemoveAll']
 
-dbmError = dbmError        # Error raised when having problems with shelve
+dbmError = error        # Error raised when having problems with shelve
+
 
 class Note:
     """ Defines the all the data that a note has and functions that return that data """
@@ -26,8 +28,7 @@ class Note:
     def __str__(self):
         return f'''
 #############################################
-ID: {self.cod}
-Title: {self.title}
+ID: {self.cod} | Title: {self.title}
 Note: {self.text}
 
 Last modification: {self.date}
@@ -69,8 +70,7 @@ class AESCipher:
                 cipher = AES.new(self.key, AES.MODE_CBC, iv)
                 print(f'''
 ###########################################
-ID: {notes_db[note_cod].cod}
-Title: {notes_db[note_cod].title}
+ID: {notes_db[note_cod].cod} | Title: {notes_db[note_cod].title}
 Text: {self.unpad(cipher.decrypt(enc[16:])).decode('UTF-8')}
 ###########################################''')
         except KeyError:
@@ -92,7 +92,7 @@ class NoteModifyWindow(tk.Frame):
         self.tk_note_text_entry.pack()
 
         self.tk_note_text_button = tk.Button(self, bg="sky blue", text="Modify", command=self.save_note_text).\
-        pack(fill='x', expand=True, padx=3, pady=3, ipadx=20, ipady=15)
+            pack(fill='x', expand=True, padx=3, pady=3, ipadx=20, ipady=15)
 
     def get_note_text(self):
         """ Returns a string with the text of the note to be modified """
@@ -102,9 +102,19 @@ class NoteModifyWindow(tk.Frame):
     def save_note_text(self):
         """ Saves the modified text to the database """
         with shelve.open("./data/notes") as notes_db:
-            notes_db[self.note_cod] = Note(notes_db[self.note_cod].cod, notes_db[self.note_cod].title, self.tk_note_text_entry.get("1.0", 'end'))
+            notes_db[self.note_cod] = Note(notes_db[self.note_cod].cod, notes_db[self.note_cod].title,
+                                           self.tk_note_text_entry.get("1.0", 'end'))
             print(notes_db[self.note_cod])
         self.quit()
+
+
+def new_note(note_title, note_text):
+    """ Creates a new Note objects and serializes it """
+    note_cod = f'n{get_total_notes()+1}'
+    with shelve.open("./data/notes") as notes_db:
+        notes_db['total'] += 1
+        notes_db[note_cod] = Note(note_cod, note_title, note_text)
+        print(notes_db[note_cod])
 
 
 def get_total_notes():
@@ -120,86 +130,70 @@ def get_total_notes():
     return total_notes
 
 
-def show_all_notes():
-    """ Prints every note that has been saved """
-    with shelve.open('./data/notes', 'r') as notes_db:
-        if len(notes_db) > 1:
-            for i in notes_db:
-                if i != "total":
-                    print(notes_db[i])
-        else:
-            print("\nThere are no notes nor reminders available")
+class CreateNote(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        new_note(values[0], values[1])
 
 
-def new_note(note_title, note_text):
-    """ Creates a new Note objects and serializes it """
-    note_cod = f'n{get_total_notes()+1}'
-    with shelve.open("./data/notes") as notes_db:
-        notes_db['total'] += 1
-        notes_db[str(note_cod)] = Note(note_cod, note_title, note_text)
-        print(notes_db[str(note_cod)])
+class CreateEncryptedNote(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        cipher = AESCipher(values[2])
+        new_note(values[0], cipher.encrypt(values[1]))
 
 
-def modify_note(note_cod):
-    try:
-        root = tk.Tk()
-        root.title("Terminal-N")
-        NoteModifyWindow(root, note_cod).pack(side="top", fill="both", expand=True)
-        root.mainloop()
-    except KeyError:
-        print(f"\nNo note with ID {note_cod}")
+class DecryptNote(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        cipher = AESCipher(values[1])
+        cipher.decrypt(values[0])
 
 
-def save(note_object, entry_object_text):
-    note_object.text = entry_object_text
-    print(note_object.text)
-
-def del_note(note_cod):
-    """ Deletes a Note object from the database selected by ID """
-    with shelve.open("./data/notes") as notes_db:
-        if notes_db.pop(str(note_cod), -1) == -1:
-            print("\nNo note with ID:", note_cod)
-
-
-def search_note(note_info):
-    """ Searches for a note in the database by ID """
-    with shelve.open('./data/notes', 'r') as notes_db:
-        if len(notes_db) > 1:
-
-            for i in notes_db:
-                if i != "total" and note_info in notes_db[i].get_data():
-                    print(notes_db[i])
-        else:
-            print("\nThere are no notes nor reminders with that data")
+class ModifyNote(argparse.Action):
+    """ Modifies an available note selected by ID """
+    def __call__(self, parser, namespace, values, option_string=None):
+        try:
+            root = tk.Tk()
+            root.title("Terminal-N")
+            NoteModifyWindow(root, values).pack(side="top", fill="both", expand=True)
+            root.mainloop()
+        except KeyError:
+            print(f"\nNo note with ID {values}")
 
 
-def del_all():
+class RemoveNote(argparse.Action):
+    """ Deletes a note from the database selected by ID """
+    def __call__(self, parser, namespace, values, option_string=None):
+        with shelve.open("./data/notes") as notes_db:
+            for note_cod in values:
+                if notes_db.pop(note_cod, -1) == -1:
+                    print("\nNo note with ID:", note_cod)
+
+
+class RemoveAll(argparse.Action):
     """ Deletes every note created """
-    with shelve.open("./data/notes", 'n') as notes_db:
-        notes_db['total'] = 0
+    def __call__(self, parser, namespace, values, option_string=None):
+        with shelve.open("./data/notes", 'n') as notes_db:
+            notes_db['total'] = 0
 
 
-def usage():
-    """ Prints the usage of nterm and examples """
-    print('''
-usage: nterm [OPTION] [<ARGUMENTS>]
+class SearchNote(argparse.Action):
+    """ Searches for a note in the database by ID, Title and string in text """
+    def __call__(self, parser, namespace, values, option_string=None):
+        with shelve.open('./data/notes', 'r') as notes_db:
+            if len(notes_db) > 1:
+                for i in notes_db:
+                    if i != "total" and values in notes_db[i].get_data():
+                        print(notes_db[i])
+            else:
+                print("\nThere are no notes nor reminders with that data")
 
-    -n, --note              Creates a new note with title and text
-    -p, --encrypt           Use this flag after a note to encrypt it with a password
-    -u, --decrypt           Use this flag to decrypt your note a the password
-    -m, --modify            Modifies a note selected by id
-    -s, --search            Searches for a note or reminder by id, title and text (CASE SENSITIVE)
-    -l, --list              Shows a list of every note and reminder sorted by modification
-    -d, --delete            Deletes an specified note or reminder selected by id
-    -D, --delete-all        Deletes EVERY note and reminder saved
-    -h, --help              Shows this help document
-        
-examples:
-    >   nterm --note "The title" "This is the text of the note"
-    >   nterm --note "The title" "This is a secret message" --encrypt "This is the password"
-    >   nterm --note n1 --decrypt "The password I used"
-    >   nterm --modify n1
-    >   nterm --list
-    >   nterm --delete n1
-    >   nterm --delete-all
-    >   nterm --search n1''')
+
+class ListAll(argparse.Action):
+    """ Prints every available note """
+    def __call__(self, parser, namespace, values, option_string=None):
+        with shelve.open('./data/notes', 'r') as notes_db:
+            if len(notes_db) > 1:
+                for i in notes_db:
+                    if i != "total":
+                        print(notes_db[i])
+            else:
+                print("\nThere are no notes nor reminders available")
---
+++ nterm.py
@@ -0,0 +1,30 @@
+from notes import *
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument("-n", "--note", action=CreateNote, nargs=2, metavar=("Title", "Text"), help="Creates a Note")
+    group.add_argument("-e", "--encrypt", action=CreateEncryptedNote, nargs=3, metavar=("Title", "Text", "Password"),
+                       help="Creates an encrypted note")
+    group.add_argument("-d", "--decrypt", action=DecryptNote, nargs=2, metavar=("ID", "Password"),
+                       help="Decrypts a note selected by Id")
+    group.add_argument("-m", "--modify", action=ModifyNote, metavar="ID", help="Modifies a note selected by Id")
+    group.add_argument("-s", "--search", action=SearchNote, metavar="ID",
+                       help="Searches for a note by Id, title and string in text")
+    group.add_argument("-l", "--list", action=ListAll, nargs=0, help="Lists every note available")
+    group.add_argument("-r", "--remove", action=RemoveNote, nargs='+', metavar="ID",
+                       help="Removes a note selected by Id")
+    group.add_argument("-R", "--remove-all", action=RemoveAll, nargs=0, help="Removes Everything")
+
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except FileNotFoundError:
+        print("\nThere are no notes available")
+    except dbmError:
+        print("\nThere are no notes available")
