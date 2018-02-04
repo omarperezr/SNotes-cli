4cdece42cbae32b14737c197f90a530db9ae735c
XSingularity <omarcorporated@gmail.com>
2018-02-03 21:35:02 -0400

    Big Update: Optimization, Better text

--- notes.py
+++ notes.py
@@ -1,6 +1,7 @@
 import argparse
 import base64
 import shelve
+import textwrap
 import tkinter as tk
 from binascii import Error as binasciiError
 from datetime import datetime
@@ -11,10 +12,14 @@ from os import mkdir
 from Crypto import Random
 from Crypto.Cipher import AES
 
-__all__ = ['dbmError', 'argparse', 'CreateNote', 'CreateEncryptedNote', 'DecryptNote', 'ModifyNote', 'SearchNote',
+__all__ = ['dbmError', 'argparse', 'CreateNote', 'CreateEncryptedNote', 'DecryptNote', 'ModifyNote', 'ShowNote',
            'ListAll', 'RemoveNote', 'RemoveAll']
 
 dbmError = error        # Error raised when having problems with shelve
+
+
+def pretty_string(string):
+    return textwrap.fill(string, width=57, replace_whitespace=False)
 
 
 class Note:
@@ -27,16 +32,25 @@ class Note:
 
     def __str__(self):
         return f'''
-#############################################
-ID: {self.cod} | Title: {self.title}
-Note: {self.text}
+ID: {self.cod}                              {self.date}
+---------------------------------------------------------
+Title: {self.title}
+    
+    {self.text}
 
-Last modification: {self.date}
-############################################'''
+---------------------------------------------------------
+'''
 
-    def get_data(self):
-        """ Returns a string with al the data in a Note """
-        return f"{self.title} {self.cod} {self.text}"
+    def shorter_str(self):
+        return f'''
+ID: {self.cod}                              {self.date}
+---------------------------------------------------------
+Title: {self.title[:20]}
+
+    {self.text[:77]}...
+
+---------------------------------------------------------
+'''
 
 
 class AESCipher:
@@ -51,28 +65,25 @@ class AESCipher:
 
     @staticmethod
     def unpad(s):
-        """ Does the inverse of pad """
         return s[0:-s[-1]]
 
     def encrypt(self, note_text):
-        """ Returns a string encrypted with AES """
         note_text = self.pad(note_text)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(note_text))
 
     def decrypt(self, note_cod):
-        """ Prints a string decrypted with AES """
         try:
             with shelve.open("./data/notes", "r") as notes_db:
                 enc = base64.b64decode(notes_db[note_cod].text)
                 iv = enc[:16]
                 cipher = AES.new(self.key, AES.MODE_CBC, iv)
                 print(f'''
-###########################################
-ID: {notes_db[note_cod].cod} | Title: {notes_db[note_cod].title}
-Text: {self.unpad(cipher.decrypt(enc[16:])).decode('UTF-8')}
-###########################################''')
+ID: {notes_db[note_cod].cod} Title: {notes_db[note_cod].title[:20]}...
+---------------------------------------------------------
+Text: {pretty_string(self.unpad(cipher.decrypt(enc[16:])).decode('UTF-8'))}
+---------------------------------------------------------''')
         except KeyError:
             print(f"\nNo note with the id {note_cod} exists")
         except binasciiError:
@@ -85,25 +96,35 @@ class NoteModifyWindow(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.note_cod = note_cod
+        self.note_title = self.get_note_title()
         self.note_text = self.get_note_text()
 
-        self.tk_note_text_entry = tk.Text(self, width=40, height=10)
+        self.var_title = tk.StringVar()
+        self.var_title.set(self.note_title)
+
+        self.tk_note_title_entry = tk.Entry(self, textvariable=self.var_title).\
+            pack(fill='x', expand=True, padx=3, pady=3, ipadx=20, ipady=2)  # Note title widget Entry
+
+        self.tk_note_text_entry = tk.Text(self, width=57, height=23)        # Note text widget Text
         self.tk_note_text_entry.insert('insert', self.note_text)
         self.tk_note_text_entry.pack()
 
-        self.tk_note_text_button = tk.Button(self, bg="sky blue", text="Modify", command=self.save_note_text).\
-            pack(fill='x', expand=True, padx=3, pady=3, ipadx=20, ipady=15)
+        self.tk_note_text_button = tk.Button(self, bg="sky blue", text="Modify", command=self.save_modified_note).\
+            pack(fill='x', expand=True, padx=3, pady=3, ipadx=20, ipady=15)  # Modify Button
 
     def get_note_text(self):
-        """ Returns a string with the text of the note to be modified """
         with shelve.open("./data/notes") as notes_db:
             return notes_db[self.note_cod].text
 
-    def save_note_text(self):
+    def get_note_title(self):
+        with shelve.open("./data/notes") as notes_db:
+            return notes_db[self.note_cod].title
+
+    def save_modified_note(self):
         """ Saves the modified text to the database """
         with shelve.open("./data/notes") as notes_db:
-            notes_db[self.note_cod] = Note(notes_db[self.note_cod].cod, notes_db[self.note_cod].title,
-                                           self.tk_note_text_entry.get("1.0", 'end'))
+            notes_db[self.note_cod] = Note(notes_db[self.note_cod].cod, self.var_title.get(),
+                                           self.tk_note_text_entry.get("1.0", 'end').rstrip('\n'))
             print(notes_db[self.note_cod])
         self.quit()
 
@@ -114,11 +135,11 @@ def new_note(note_title, note_text):
     with shelve.open("./data/notes") as notes_db:
         notes_db['total'] += 1
         notes_db[note_cod] = Note(note_cod, note_title, note_text)
-        print(notes_db[note_cod])
+        print(f'\n{notes_db[note_cod]}')
 
 
 def get_total_notes():
-    """ Returns the total of notes created """
+    """ Returns the total of notes created and creates the data folder if 1rst time use"""
     try:
         with shelve.open('./data/notes', 'r') as notes_db:
             total_notes = notes_db['total']
@@ -128,6 +149,8 @@ def get_total_notes():
             notes_db["total"] = total_notes = 0
 
     return total_notes
+
+# Custom Argparse Functions
 
 
 class CreateNote(argparse.Action):
@@ -149,18 +172,18 @@ class DecryptNote(argparse.Action):
 
 class ModifyNote(argparse.Action):
     """ Modifies an available note selected by ID """
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(self, parser, namespace, value, option_string=None):
         try:
             root = tk.Tk()
-            root.title("Terminal-N")
-            NoteModifyWindow(root, values).pack(side="top", fill="both", expand=True)
+            root.title(f"Note {value}")
+            NoteModifyWindow(root, value).pack(side="top", fill="both", expand=True)
             root.mainloop()
         except KeyError:
-            print(f"\nNo note with ID {values}")
+            print(f"\nNo note with ID {value}")
 
 
 class RemoveNote(argparse.Action):
-    """ Deletes a note from the database selected by ID """
+    """ Deletes n notes from the database selected by ID """
     def __call__(self, parser, namespace, values, option_string=None):
         with shelve.open("./data/notes") as notes_db:
             for note_cod in values:
@@ -175,25 +198,25 @@ class RemoveAll(argparse.Action):
             notes_db['total'] = 0
 
 
-class SearchNote(argparse.Action):
+class ShowNote(argparse.Action):
     """ Searches for a note in the database by ID, Title and string in text """
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(self, parser, namespace, value, option_string=None):
         with shelve.open('./data/notes', 'r') as notes_db:
             if len(notes_db) > 1:
-                for i in notes_db:
-                    if i != "total" and values in notes_db[i].get_data():
-                        print(notes_db[i])
+                try:
+                    print(f'\n{pretty_string(str(notes_db[value]))}')
+                except KeyError:
+                    print(f"\nThere is no note with ID {value}")
             else:
-                print("\nThere are no notes nor reminders with that data")
+                print("\nThere are no notes available")
 
 
 class ListAll(argparse.Action):
-    """ Prints every available note """
+    """ Shows every available note """
     def __call__(self, parser, namespace, values, option_string=None):
         with shelve.open('./data/notes', 'r') as notes_db:
             if len(notes_db) > 1:
-                for i in notes_db:
-                    if i != "total":
-                        print(notes_db[i])
+                for i in list(notes_db.values())[1:]:
+                    print(f'\n{pretty_string(i.shorter_str())}')
             else:
-                print("\nThere are no notes nor reminders available")
+                print("\nThere are no notes available")
--- nterm.py
+++ nterm.py
@@ -9,13 +9,13 @@ def main():
     group.add_argument("-e", "--encrypt", action=CreateEncryptedNote, nargs=3, metavar=("Title", "Text", "Password"),
                        help="Creates an encrypted note")
     group.add_argument("-d", "--decrypt", action=DecryptNote, nargs=2, metavar=("ID", "Password"),
-                       help="Decrypts a note selected by Id")
+                       help="Decrypts a note selected by ID")
     group.add_argument("-m", "--modify", action=ModifyNote, metavar="ID", help="Modifies a note selected by Id")
-    group.add_argument("-s", "--search", action=SearchNote, metavar="ID",
-                       help="Searches for a note by Id, title and string in text")
+    group.add_argument("-s", "--show", action=ShowNote, metavar="ID",
+                       help="Shows a note selected by ID")
     group.add_argument("-l", "--list", action=ListAll, nargs=0, help="Lists every note available")
     group.add_argument("-r", "--remove", action=RemoveNote, nargs='+', metavar="ID",
-                       help="Removes a note selected by Id")
+                       help="Removes a note selected by ID")
     group.add_argument("-R", "--remove-all", action=RemoveAll, nargs=0, help="Removes Everything")
 
     parser.parse_args()
